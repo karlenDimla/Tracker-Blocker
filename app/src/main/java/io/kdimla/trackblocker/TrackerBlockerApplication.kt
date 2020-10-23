c9b863f992014949d93c3b193b84e4d4ccc099eb
Karl Dimla <karllenmae@spotify.com>
2020-10-23 15:07:03 +0200

    Load data on application create

--- app/src/main/java/io/kdimla/trackblocker/TrackerBlockerApplication.kt
+++ app/src/main/java/io/kdimla/trackblocker/TrackerBlockerApplication.kt
@@ -2,6 +2,14 @@ package io.kdimla.trackblocker
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import io.kdimla.trackblocker.trackerdata.source.TrackerDataLoader
+import javax.inject.Inject
 
 @HiltAndroidApp
-class TrackerBlockerApplication: Application()
+class TrackerBlockerApplication: Application() {
+    @Inject lateinit var trackerDataLoader: TrackerDataLoader
+    override fun onCreate() {
+        super.onCreate()
+        trackerDataLoader.loadData()
+    }
+}
---
+++ app/src/main/java/io/kdimla/trackblocker/trackerdata/di/TrackerDataModule.kt
@@ -0,0 +1,40 @@
+package io.kdimla.trackblocker.trackerdata.di
+
+import android.app.Application
+import android.content.Context
+import com.squareup.moshi.Moshi
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import io.kdimla.trackblocker.trackerdata.source.TrackerDataLoader
+import io.kdimla.trackblocker.trackerdata.source.TrackerDataParser
+import io.kdimla.trackblocker.trackerdata.source.disconnect.DisconnectDataLoader
+import io.kdimla.trackblocker.trackerdata.source.disconnect.DisconnectEntitiesParser
+import io.kdimla.trackblocker.trackerdata.source.disconnect.model.TrackerEntitiesAdapter
+import io.kdimla.trackblocker.trackerdata.source.disconnect.model.TrackerServicesAdapter
+import javax.inject.Named
+
+@Module
+@InstallIn(ApplicationComponent::class)
+interface TrackerDataModule {
+    companion object {
+        @Provides
+        fun provideMoshi(): Moshi = Moshi.Builder()
+            .add(TrackerServicesAdapter())
+            .add(TrackerEntitiesAdapter())
+            .build()
+
+    }
+    @Binds
+    fun bindTrackerDataParser(impl: DisconnectEntitiesParser): TrackerDataParser
+
+    @Binds
+    fun bindTrackerDataLoader(impl: DisconnectDataLoader): TrackerDataLoader
+
+    @Binds
+    @Named("applicationContext")
+    fun bindAppContext(application: Application): Context
+
+}
