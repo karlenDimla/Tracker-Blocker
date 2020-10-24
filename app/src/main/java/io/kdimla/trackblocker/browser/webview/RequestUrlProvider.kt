8997153dc4fe5dd18bfe6657b8686b8bd5723af9
Karl Dimla <karllenmae@spotify.com>
2020-10-24 14:31:11 +0200

    Add unit test for RequestUrlProviderImplTest

--- app/src/main/java/io/kdimla/trackblocker/browser/webview/RequestUrlProvider.kt
+++ app/src/main/java/io/kdimla/trackblocker/browser/webview/RequestUrlProvider.kt
@@ -1,6 +1,9 @@
 package io.kdimla.trackblocker.browser.webview
 
 import android.util.Patterns
+import androidx.core.util.PatternsCompat
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 import javax.inject.Inject
 
 interface RequestUrlProvider {
@@ -9,7 +12,7 @@ interface RequestUrlProvider {
 
 class RequestUrlProviderImpl @Inject constructor() : RequestUrlProvider {
     companion object {
-        private val webUrlPattern = Patterns.WEB_URL
+        private val webUrlPattern = PatternsCompat.WEB_URL
         private const val DEFAULT_SEARCH_URL = "https://duckduckgo.com/"
         private const val PREFIX_HTTPS = "https://"
         private const val PREFIX_HTTP = "http://"
@@ -17,10 +20,12 @@ class RequestUrlProviderImpl @Inject constructor() : RequestUrlProvider {
 
     override fun getUrl(searchText: String?): String {
         val isUrl = searchText?.isUrl() ?: return DEFAULT_SEARCH_URL
-        return if (isUrl) searchText.appendHttps() else "$DEFAULT_SEARCH_URL?q=$searchText"
+        return if (isUrl) searchText.appendHttps() else "$DEFAULT_SEARCH_URL?q=${searchText.encode()}"
     }
 
     private fun String.isUrl(): Boolean = webUrlPattern.matcher(this).matches()
+
+    private fun String.encode(): String = URLEncoder.encode(this, StandardCharsets.UTF_8.toString())
 
     private fun String.appendHttps(): String {
         if (startsWith(PREFIX_HTTPS)) return this
--- app/src/test/java/io/kdimla/trackblocker/browser/webview/RequestUrlProviderImplTest.kt
+++ app/src/test/java/io/kdimla/trackblocker/browser/webview/RequestUrlProviderImplTest.kt
@@ -1,5 +1,51 @@
 package io.kdimla.trackblocker.browser.webview
 
-import junit.framework.TestCase
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
 
-class RequestUrlProviderImplTest : TestCase()
+class RequestUrlProviderImplTest {
+
+    private val underTest = RequestUrlProviderImpl()
+
+    @Test
+    fun nullUrl_getUrl_returnSearchUrl() {
+        val result = underTest.getUrl(null)
+
+        assertThat(result).isEqualTo("https://duckduckgo.com/")
+    }
+
+    @Test
+    fun emptyUrl_getUrl_returnSearchUrl() {
+        val result = underTest.getUrl("")
+
+        assertThat(result).isEqualTo("https://duckduckgo.com/?q=")
+    }
+
+    @Test
+    fun notUrl_getUrl_returnSearchUrlWithQuery() {
+        val result = underTest.getUrl("This is not a url")
+
+        assertThat(result).isEqualTo("https://duckduckgo.com/?q=This+is+not+a+url")
+    }
+
+    @Test
+    fun isUrlNotHttps_getUrl_returnUrlWithHttps() {
+        val result = underTest.getUrl("facebook.com")
+
+        assertThat(result).isEqualTo("https://facebook.com")
+    }
+
+    @Test
+    fun isUrlWithHttps_getUrl_returnUrl() {
+        val result = underTest.getUrl("https://facebook.com")
+
+        assertThat(result).isEqualTo("https://facebook.com")
+    }
+
+    @Test
+    fun isUrlWithHttp_getUrl_returnUrlWithHttps() {
+        val result = underTest.getUrl("http://facebook.com")
+
+        assertThat(result).isEqualTo("https://facebook.com")
+    }
+}
